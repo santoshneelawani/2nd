cacad390dc7f341c87cc5fc90c18dfad716b18ba
Santosh Neelawani <135528710+santoshneelawani@users.noreply.github.com>
2023-10-05 13:15:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function validate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if (username === password && username.length > 1) {
+    if(username === username && password==password && username.length > 1)
+     {
       alert("Login Successful!");
       window.localStorage.setItem("loginStatus", true);
-      window.location.href = "order.html"; 
+      window.location.href = "main_page.html"; 
     } else {
       alert("Please enter valid credentials!");
       window.location.href = "index.html"; 
